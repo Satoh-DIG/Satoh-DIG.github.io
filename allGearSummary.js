b0b6b73d98e781c28aeb597806e4312821abb0d8
Satoh-DIG <124141304+Satoh-DIG@users.noreply.github.com>
2023-03-26 22:21:48 +0900

    Create allGearSummary.js

---
+++ allGearSummary.js
@@ -0,0 +1,29 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+const allGearSummary = [
+    {
+        summaryId: "S-001",
+        planetaryType: "SINGLE",
+        gearSun: 20,
+        gearPinion: 18,
+        gearRing: 50,
+        numPin: 4,
+    },
+    {
+        summaryId: "S-002",
+        planetaryType: "SINGLE",
+        gearSun: 22,
+        gearPinion: 19,
+        gearRing: 51,
+        numPin: 3,
+    },
+    {
+        summaryId: "G-101",
+        planetaryType: "SINGLE",
+        gearSun: 21,
+        gearPinion: 17,
+        gearRing: 53,
+        numPin: 4,
+    }
+];
